84a0bef86ff55b94391f96e5c89d248fffd894f1
Nancy-GDA <nancy.glez.dea@gmail.com>
2024-03-11 22:10:44 -0600

    Maquetacion del login

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Form = styled.form`
+    border:1px solid red;
+    width: 400px;
+    height:500px;
+
+`
+
+export default Form
---
+++ src/login/login.jsx
@@ -0,0 +1,13 @@
+import Form from '../components/Form/index'
+
+const Login  = () => {
+    return (
+        <>
+        <Form>
+        <h2>Login</h2>
+        </Form>
+        </>
+    )
+}
+
+export default Login
