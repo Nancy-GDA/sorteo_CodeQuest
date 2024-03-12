c030905608036029edd74b7737d86dbf487337ad
Nancy-GDA <nancy.glez.dea@gmail.com>
2024-03-12 17:33:42 -0600

    Login con estilos

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,10 +1,21 @@
-import Form from '../components/Form/index'
+import Form, {Title,ContainerInput, Input, Button, Label} from '../components/Form/index'
 
 const Login  = () => {
     return (
         <>
-        <Form>
-        <h2>Login</h2>
+        <Form className='form-control'>
+  <Title> Login</Title>
+  <ContainerInput className="input-field">
+    <Input required="" className="input" type="text" />
+    <Label className="label" htmlFor="input">Enter Email</Label>
+  </ContainerInput>
+  <ContainerInput className="input-field">
+    <Input required="" className="input" type="password" />
+    <Label className="label" htmlFor="input">Enter Password</Label>
+  </ContainerInput>
+  <a>Forgot your password?</a>
+  <Button className="submit-btn">Sign In</Button>
+
         </Form>
         </>
     )
